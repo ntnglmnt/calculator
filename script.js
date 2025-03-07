cd47b45107b5fdb76300c2292fbd92a11bb24918
doomsday <intniogi@hotmail.com>
2025-03-07 10:44:20 +0200

    calculator project day 1

---
+++ script.js
@@ -0,0 +1,92 @@
+let main = document.createElement('div');
+document.appendChild(main);
+
+let display = document.createElement('div')
+main.appendChild(display);
+
+let buttonGrid = document.createElement('div')
+main.appendChild(buttonGrid);
+
+let buttonAC = document.createElement('button')
+buttonGrid.value = 'AC'
+buttonAC.classList.add('operator')
+buttonGrid.appendChild(buttonAC)
+
+let buttonSign = document.createElement('button')
+buttonSign.value = ''
+buttonSign.classList.add('operator')
+buttonGrid.appendChild(buttonSign)
+
+let buttonPercent = document.createElement('button')
+buttonPercent.value = '%'
+buttonPercent.classList.add('operator')
+buttonGrid.appendChild(buttonPercent)
+
+let buttonDivision = document.createElement('button')
+buttonDivision.value = '/'
+buttonDivision.classList.add('operator')
+buttonGrid.appendChild(buttonDivision)
+
+let buttonSeven = document.createElement('button')
+buttonSeven.value = '7'
+buttonSeven.classList.add('operand')
+buttonGrid.appendChild(buttonSeven)
+
+let buttonEight = document.createElement('button')
+buttonEight.value = '8'
+buttonEight.classList.add('operand')
+buttonGrid.appendChild(buttonEight)
+
+let buttonNine = document.createElement('button')
+buttonNine.value = '9'
+buttonNine.classList.add('operand')
+buttonGrid.appendChild(buttonNine)
+
+let buttonMultiply = document.createElement('button')
+buttonMultiply.value = '*'
+buttonMultiply.classList.add('operator')
+buttonGrid.appendChild(buttonMultiply)
+
+let buttonFour = document.createElement('button')
+buttonFour.value = '4'
+buttonFour.classList.add('operand')
+buttonGrid.appendChild(buttonFour)
+
+let buttonFive = document.createElement('button')
+buttonFive.value = '5'
+buttonFive.classList.add('operand')
+buttonGrid.appendChild(buttonFive)
+
+let buttonSix = document.createElement('button')
+buttonSix.value = '6'
+buttonGrid.appendChild(buttonSix)
+
+let buttonMinus = document.createElement('button')
+buttonMinus.value = '-'
+buttonMinus.classList.add('operator')
+buttonGrid.appendChild(buttonMinus)
+
+let buttonOne = document.createElement('button')
+buttonOne.value = '1'
+buttonOne.classList.add('operand')
+buttonGrid.appendChild(buttonOne)
+
+let buttonTwo = document.createElement('button')
+buttonTwo.value = '2'
+buttonTwo.classList.add('operand')
+buttonGrid.appendChild(buttonTwo)
+
+let buttonThree = document.createElement('button')
+buttonThree.value = '3'
+buttonThree.classList.add('operand')
+buttonGrid.appendChild(buttonThree)
+
+let buttonPlus = document.createElement('button')
+buttonPlus.value = '+'
+buttonPlus.classList.add('operator')
+buttonGrid.appendChild(buttonPlus)
+
+let buttonZero = document.createElement('button')
+buttonZero.value = '0'
+buttonZero.classList.add('operand')
+buttonGrid.appendChild(buttonZero)
